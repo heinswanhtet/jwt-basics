8917ab0b114d9f18c01502ec365662313214a1f2
Hein Swan Htet <hshscientist@gmail.com>
2024-03-14 16:08:37 +0630

    Fixed: changed from GET to POST method correctly in login route

--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { login, getCats } = require('../controllers/main')
 const authenticationMiddleware = require('../middleware/auth')
 
-router.route('/login').get(login)
+router.route('/login').post(login)
 router.route('/cats').get(authenticationMiddleware, getCats)
 
 module.exports = router
